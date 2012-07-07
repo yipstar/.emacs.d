47d2d26a32762e4de30c9832ebaf20bc021ecae6
Gianni Jacklone <gianni.jacklone@gmail.com>
2012-07-06 17:41:45 -0700

    use nxhtml for rhtml files

--- yipstar/packages.el
+++ yipstar/packages.el
@@ -43,3 +43,4 @@
 (yipstar-install-packages)
 
 
+
--- yipstar/ruby.el
+++ yipstar/ruby.el
@@ -2,9 +2,21 @@
 (require 'rinari-autoloads)
 (setq rinari-tags-file-name "TAGS")
 
-;; rhtml
-(setq auto-mode-alist (cons '("\\.html\\.erb" . nxml-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.erb" . nxml-mode) auto-mode-alist))
+;; rhtml with nxml
+;; (setq auto-mode-alist (cons '("\\.html\\.erb" . nxml-mode) auto-mode-alist))
+;; (setq auto-mode-alist (cons '("\\.erb" . nxml-mode) auto-mode-alist))
+
+;; rhtml with nxhtml
+(load "nxhtml/autostart.el")
+(setq
+ nxhtml-global-minor-mode t
+ mumamo-chunk-coloring 'submode-colored
+ nxhtml-skip-welcome t
+ indent-region-mode t
+ rng-nxml-auto-validate-flag nil
+ nxml-degraded t)
+(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . eruby-nxhtml-mumamo-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . eruby-nxhtml-mumamo-mode))
 
 ;; ruby
 (setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
