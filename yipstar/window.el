5e0525fbcfef9d26c7a4d182411c4c6a56bf66c3
Gianni Jacklone <gianni.jacklone@gmail.com>
2012-07-10 09:03:02 -0700

    add my common window split functions

--- yipstar/window.el
+++ yipstar/window.el
@@ -12,3 +12,37 @@
 (require 'window-layout-autoloads)
 (require 'e2wm-autoloads)
 (require 'e2wm)
+
+;; Some common window split configs.
+(defun 4-equal-window-view ()
+  "split current-frame into four equally sized windows"
+  (interactive)
+  (delete-other-windows)
+  (split-window-horizontally)
+  (split-window-vertically)
+  (other-window 2)
+  (split-window-vertically))
+
+(defun 3by2-window-view ()
+  "split current-frame into 2 by 3 equally sized windows"
+  (interactive)
+  (delete-other-windows)
+  (setq window_width (/ (frame-width) 3))
+  (message "setting window width to %s" window_width)
+  (split-window-horizontally window_width)
+  (other-window 1)
+  (split-window-horizontally window_width)
+  (split-window-vertically)
+  (other-window 2)
+  (split-window-vertically)
+  (other-window 2)
+  (split-window-vertically))
+
+(defun 3by2mod4-window-view ()
+  "split current-frame into 8 windows (irc setup)"
+  (interactive)
+  (3by2-window-view)
+  (other-window 4)
+  (split-window-vertically)
+  (other-window 2)
+  (split-window-vertically))
