fe43bf943c78e0352928ad883f5bbac7ef1af199
Gianni Jacklone <gianni.jacklone@gmail.com>
2012-08-07 18:45:57 -0700

    add yaml-mode

--- yipstar/modes.el
+++ yipstar/modes.el
@@ -15,7 +15,19 @@
 (load "yipstar/flymake")
 
 ;; twittering
+;; TODO: turn on auto word wrap in twittering-mode
 (require 'twittering-mode-autoloads)
 
 ;; electrir-pair-mode for completing quotes, etc
 (electric-pair-mode t)
+
+;; objc-mode
+(add-hook 'objc-mode-hook (lambda () (subword-mode t)))
+
+;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/vendor/senny-emacs-xcode/"))
+;; (require 'xcode)
+
+(load "yipstar/xcodebuild")
+
+;; yaml-mode
+(require 'yaml-mode-autoloads)
