a5702da0b734712fc677645d7d71d74eee88bbde
Gianni Jacklone <gianni.jacklone@gmail.com>
2012-12-30 09:36:33 -0500

    add exec-path-from-shell to finally fix my shell PATH issues

--- yipstar/packages.el
+++ yipstar/packages.el
@@ -22,7 +22,7 @@
   '(ack-and-a-half twilight-theme zenburn-theme rinari switch-window scss-mode js2-mode window-layout e2wm
 		   maxframe rvm yari textmate flymake-cursor flymake-ruby flymake-jslint 
 		   twittering-mode yaml-mode magit csv-mode github-theme solarized-theme color-theme-sanityinc-tomorrow
-                   ess)
+                   ess exec-path-from-shell)
   "A list of packages to ensure are installed at launch.")
 
 (defun yipstar-packages-installed-p ()
--- yipstar/shell.el
+++ yipstar/shell.el
@@ -1,9 +1,12 @@
+;; This keeps my PATH from bash working :)
+(exec-path-from-shell-initialize)
+
 ;; TODO: keeping a duplicate PATH for emacs sucks, why isn't the plist fix working?
 ;; http://olabini.com/blog/2009/12/path-problem-with-emacs-on-mac-os-x/
 
-(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-(setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
+;; (setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
+;; (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+;; (setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
 
 (require 'ansi-color)
 (defun colorize-compilation-buffer ()
@@ -14,3 +17,5 @@
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 (setq ansi-color-for-comint-mode t)
 (add-hook 'minibuffer-setup-hook 'colorize-compilation-buffer)
+
+
