831675171bbdabc986b768042600cbf5fef03f6d
Gianni Jacklone <gianni.jacklone@gmail.com>
2013-11-20 21:41:16 -0800

    use wombat theme and clean up my theme file a little

--- yipstar/theme.el
+++ yipstar/theme.el
@@ -1,18 +1,12 @@
-;; TODO: is this necessary anymore in emacs 24?
-;; (setq color-theme-is-global t)
-
-(load-theme 'twilight)
-;;(load-theme 'zenburn)
+(load-theme 'wombat)
+;;(load-theme 'twilight)
+;; (load-theme 'zenburn)
 
 (add-to-list 'custom-theme-load-path "/Users/yipstar/.emacs.d/elpa/solarized-theme-20121023.2059/")
 ;; (load-theme 'solarized-light)
 
 (add-to-list 'custom-theme-load-path "/Users/yipstar/.emacs.d/elpa/github-theme-0.0.3/")
 ;; (load-theme 'github)
-
-;; (add-to-list 'custom-theme-load-path "/Users/yipstar/.emacs.d/elpa/color-theme-sanityinc-tomorrow-20121101.1043/")
-;; (require 'color-theme-sanityinc-tomorrow-autoloads)
-;; (load-theme 'color-theme-sanityinc-tommorow-day)
 
 ;; TODO: where is jw's theme?
 ;; (load-theme 'jw-dark) 
